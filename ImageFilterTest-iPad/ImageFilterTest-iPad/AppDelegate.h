fdd8251ccf6c0bcf9055332cb1ba1554fbf471e8
Muhammad Naviwala <majidnaviwala@gmail.com>
2014-10-25 22:57:40 -0500

    initial commit

---
+++ ImageFilterTest-iPad/ImageFilterTest-iPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageFilterTest-iPad
+//
+//  Created by Muhammad Naviwala on 10/25/14.
+//  Copyright (c) 2014 Muhammad Naviwala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageFilterTest-iPad/ImageFilterTest-iPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageFilterTest-iPad
+//
+//  Created by Muhammad Naviwala on 10/25/14.
+//  Copyright (c) 2014 Muhammad Naviwala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
